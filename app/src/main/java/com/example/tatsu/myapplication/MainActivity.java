2662fc723fec6d3eb5b0c9b12cb0a5e132e08092
tatsu19 <peertopeer0430@yahoo.co.jp>
2018-03-06 01:37:39 +0900

    コメントを追加

--- app/src/main/java/com/example/tatsu/myapplication/MainActivity.java
+++ app/src/main/java/com/example/tatsu/myapplication/MainActivity.java
@@ -11,6 +11,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
+//コミットできたよ
 
 public class MainActivity extends AppCompatActivity {
 
